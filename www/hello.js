b174753115c4f6016d0026d7d7b91a96f2deee33
tomcat6 <tomcat6@sam1-core-app1>
2016-05-20 18:59:40 +0000

    Initialize app from template

---
+++ www/hello.js
@@ -0,0 +1,17 @@
+document.getElementById('say_hello').onclick = function () {
+  document.getElementById('cloudResponse').innerHTML = "<p>Calling Cloud.....</p>";
+  $fh.cloud(
+      {
+        path: 'hello',
+        data: {
+          hello: document.getElementById('hello_to').value
+        }
+      },
+      function (res) {
+        document.getElementById('cloudResponse').innerHTML = "<p>" + res.msg + "</p>";
+      },
+      function (code, errorprops, params) {
+        alert('An error occured: ' + code + ' : ' + errorprops);
+      }
+  );
+};

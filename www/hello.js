65eee8a73e13fe3d634045df9a144a8059378936
Jim 'Wuz Here' Dillon <jdillon@jdillon-osx.local>
2016-05-23 08:14:56 -0400

    add function to refresh token

--- www/hello.js
+++ www/hello.js
@@ -1,8 +1,40 @@
 var auth;
+var keycloakAuth;
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
+function callCloud(route) {
+  keycloakAuth.updateToken(30)
+    .success(function() {
+      if(keycloakAuth && keycloakAuth.token) {
+        auth.token = keycloakAuth.token;
+        $fh.cloud(
+            {
+              path: 'keycloak-demo/' + route,
+              method: 'GET',
+              headers: {
+                'Authorization': 'Bearer ' + auth.token
+              },
+              timeout: 25000
+            },
+            function (res) {
+              document.getElementById('cloudResponse').innerHTML = '<p>' + res.msg + '</p>';
+            },
+            function (code, errorprops, params) {
+              document.getElementById('cloudResponse').innerHTML = '<p class="auth_error">' + code + '</p>';
+            }
+        );
+      }
+    })
+    .error(function() {
+        document.getElementById('cloudResponse').innerHTML = '<p class="auth_error">Error updating token.</p>';
+    });
+
+
+}
+
 function onDeviceReady() {
-  var keycloakAuth = new Keycloak('keycloak.json');
+  keycloakAuth = new Keycloak('keycloak.json');
   keycloakAuth.init({
     onLoad: 'login-required'
   })
@@ -19,22 +51,22 @@ function onDeviceReady() {
   });
 }
 
-document.getElementById('say_hello').onclick = function () {
-  document.getElementById('cloudResponse').innerHTML = '<p>Calling Cloud.....</p>';
-  $fh.cloud(
-      {
-        path: 'keycloak-demo/needs-authentication',
-        method: 'GET',
-        headers: {
-          'Authorization': 'Bearer ' + auth.token
-        },
-        timeout: 25000
-      },
-      function (res) {
-        document.getElementById('cloudResponse').innerHTML = '<p>' + res.msg + '</p>';
-      },
-      function (code, errorprops, params) {
-        alert('An error occured: ' + code + ' : ' + errorprops);
-      }
-  );
+document.getElementById('noAuthentication').onclick = function () {
+  callCloud('no-authentication-needed');
+};
+
+document.getElementById('authentication').onclick = function () {
+  callCloud('needs-authentication');
+};
+
+document.getElementById('userRole').onclick = function () {
+  callCloud('needs-user-role');
+};
+
+document.getElementById('supervisorRole').onclick = function () {
+  callCloud('needs-supervisor-role');
+};
+
+document.getElementById('adminRole').onclick = function () {
+  callCloud('needs-admin-role');
 };
